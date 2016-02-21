dc5d3453b891cc74f10b82a5464e96e0017275f9
Rodolfo <rodolfog.peixoto@gmail.com>
2016-02-21 12:30:46 -0300

    Settings Cache in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,6 +46,7 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.serve_static_files = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
@@ -79,7 +80,7 @@ Rails.application.configure do
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
-  config.static_cache_control = "public, max-age=3600"
+  config.static_cache_control = "public, max-age=2592000"
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
