3eef29ce1b7d709f7e96f5e9cfdded96b0f1f13c
Rodolfo <rodolfog.peixoto@gmail.com>
2016-02-21 12:05:10 -0300

    Modified cache in production.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,8 +78,8 @@ Rails.application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
   # Use a different cache store in production.
-  # config.cache_store = :mem_cache_store
-  config.static_cache_control = "public, max-age=3600"
+  config.cache_store = :mem_cache_store
+  config.static_cache_control = "public, max-age=2592000"
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
