b9e03586de24efa57bc6fa08f9e2142bcaa8eb91
Rodolfo <rodolfog.peixoto@gmail.com>
2016-02-20 23:47:35 -0200

    Fixed forget password with gmail smtp.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,8 +14,27 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+
 
    GA.tracker = "UA-62025133-2"
+
+   config.action_mailer.default_url_options = { :host => 'lovet.herokuapp.com' }
+
+   config.action_mailer.smtp_settings = {
+      address: "smtp.gmail.com",
+      port: 587,
+      domain: ENV["GMAIL_DOMAIN"],
+      authentication: "plain",
+      enable_starttls_auto: true,
+      user_name: ENV["GMAIL_USERNAME"],
+      password: ENV["GMAIL_PASSWORD"]
+   }
+
+
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
